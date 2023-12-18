914e112372aa6c2c6110654e551ba24eccb5a1bf
deepwubs <pchucklambs@gmail.com>
2023-12-18 16:41:36 -0600

    added objects and parallax responsiveness

--- 02-classic-techniques/19-scroll-based-animation/src/script.js
+++ 02-classic-techniques/19-scroll-based-animation/src/script.js
@@ -4,14 +4,19 @@ import GUI from 'lil-gui'
 /**
  * Debug
  */
-const gui = new GUI()
+const gui = new GUI({
+    width: 300
+})
 
 const parameters = {
-    materialColor: '#ffeded'
+    materialColor: '#188161'
 }
 
 gui
     .addColor(parameters, 'materialColor')
+    .onChange(() => {
+        material.color.set(parameters.materialColor)
+    })
 
 /**
  * Base
@@ -25,22 +30,73 @@ const scene = new THREE.Scene()
 /**
  * Test cube
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial({ color: '#ff0000' })
+// )
+// scene.add(cube)
+
+// 
+// Objects
+// 
+// Textures
+const textureLoader = new THREE.TextureLoader()
+const gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
+gradientTexture.magFilter = THREE.NearestFilter
+
+
+// Material
+const material = new THREE.MeshToonMaterial({
+    color: parameters.materialColor,
+    gradientMap: gradientTexture,
+    // wireframe: true
+})
+
+// Mesh
+const objectsDistance = 4 
+const mesh1 = new THREE.Mesh(
+    new THREE.TorusGeometry(1, .4, 16, 60),
+    material
 )
-scene.add(cube)
+mesh1.position.x = 2
+
+const mesh2 = new THREE.Mesh(
+    new THREE.ConeGeometry(1, 2, 32),
+    material
+)
+mesh2.position.x = -2
+
+const mesh3 = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(.8, .35, 100, 16),
+    material
+)
+mesh3.position.x = 2
+
+mesh1.position.y = - objectsDistance * 0
+mesh2.position.y = - objectsDistance * 1
+mesh3.position.y = - objectsDistance * 2
+
+scene.add(mesh1, mesh2, mesh3)
+
+const sectionMeshes = [mesh1, mesh2, mesh3]
+
+//
+// Lights
+//
+const directionalLight = new THREE.DirectionalLight('#ffffff', 3)
+directionalLight.position.set(1, 1, 2)
+scene.add(directionalLight)
+
 
 /**
  * Sizes
- */
+*/
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -57,28 +113,71 @@ window.addEventListener('resize', () =>
 /**
  * Camera
  */
+// Group
+const cameraGroup = new THREE.Group()
+scene.add(cameraGroup)
+
 // Base camera
 const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 6
-scene.add(camera)
+cameraGroup.add(camera)
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true,
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+//
+// Scroll
+//
+let scrollY = window.scrollY
+
+window.addEventListener('scroll', () => {
+    scrollY = window.scrollY
+})
+
+//
+// Cursor
+//
+const cursor = {}
+cursor.x = 0
+cursor.y = 0
+
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = event.clientY / sizes.height - 0.5
+})
 
 /**
  * Animate
  */
 const clock = new THREE.Clock()
+let previousTime = 0
 
-const tick = () =>
-{
+const tick = () => {
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime
+    previousTime = elapsedTime
+
+    // Animate Camera
+    camera.position.y = - scrollY / sizes.height * objectsDistance
+
+    const parallaxX = cursor.x * .5
+    const parallaxY = - cursor.y * .5
+    const parallaxStrength = 5
+    cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * parallaxStrength * deltaTime
+    cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * parallaxStrength * deltaTime
+
+    // Animate Meshes
+    for(const mesh of sectionMeshes) {
+        mesh.rotation.x = elapsedTime * -0.1
+        mesh.rotation.y = elapsedTime * 0.12
+    }
 
     // Render
     renderer.render(scene, camera)
