3426d41c947f8ebd9f5ff2a67565f8f9aa383b5a
pclambs <pchucklambs@gmail.com>
2024-03-01 13:49:47 -0600

    completed lesson 23-custom-blender-models

--- 03-advanced-techniques/23-custom-blender-models/src/script.js
+++ 03-advanced-techniques/23-custom-blender-models/src/script.js
@@ -28,7 +28,7 @@ gltfLoader.setDRACOLoader(dracoLoader)
 let mixer = null
 
 gltfLoader.load(
-    '/models/hamburger.glb',
+    '/models/burger.glb',
     (gltf) =>
     {
         scene.add(gltf.scene)
@@ -53,10 +53,10 @@ scene.add(floor)
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 2.4)
+const ambientLight = new THREE.AmbientLight(0xffffff, 1)
 scene.add(ambientLight)
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1.8)
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1)
 directionalLight.castShadow = true
 directionalLight.shadow.mapSize.set(1024, 1024)
 directionalLight.shadow.camera.far = 15
